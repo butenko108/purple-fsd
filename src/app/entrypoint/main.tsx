8ca80131111e6c25f79b487e61d1662357435cfb
Oleg Butenko <butenko@netronic.net>
2026-06-25 12:27:16 +0300

    chore: create app layer

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -1,14 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <div>Hello World</div>,
-	},
-]);
+import { router } from "../routes";
 
 const root = document.getElementById("root");
 
