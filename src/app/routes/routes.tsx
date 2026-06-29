cae3b41217087bbfd1dee51f609218d291a19337
Oleg Butenko <butenko@netronic.net>
2026-06-29 15:43:55 +0300

    setup steiger

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router";
-import { HomePage } from "../../pages";
+import { HomePage } from "../../pages/HomePage";
 import { Layout } from "../layout/Layout";
 
 export const router = createBrowserRouter([
